f3c6b2af3362226f4dcde86bd911ffa7c1f8faed
Z <your@example.com>
2018-04-21 22:28:06 +0600

    Commit #2

---
+++ database.sql
@@ -0,0 +1,85 @@
+﻿--
+-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 6.3.341.0
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 21.04.2018 22:20:15
+-- Версия сервера: 5.5.5-10.1.19-MariaDB
+-- Версия клиента: 4.1
+--
+
+
+-- 
+-- Отключение внешних ключей
+-- 
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
+-- 
+-- Установить режим SQL (SQL mode)
+-- 
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 
+-- Установка кодировки, с использованием которой клиент будет посылать запросы на сервер
+--
+SET NAMES 'utf8';
+
+-- 
+-- Установка базы данных по умолчанию
+--
+USE `database`;
+
+--
+-- Описание для таблицы tasks
+--
+DROP TABLE IF EXISTS tasks;
+CREATE TABLE tasks (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(50) DEFAULT NULL,
+  text TEXT DEFAULT NULL,
+  file VARCHAR(255) DEFAULT NULL,
+  checked INT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 2
+AVG_ROW_LENGTH = 16384
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+--
+-- Описание для таблицы users
+--
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) DEFAULT NULL,
+  password VARCHAR(255) DEFAULT NULL,
+  token VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 2
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+-- 
+-- Вывод данных для таблицы tasks
+--
+INSERT INTO tasks VALUES
+(1, 'ds', 'sd@s.f', 'ds', 'Без названия.jpg', 0);
+
+-- 
+-- Вывод данных для таблицы users
+--
+INSERT INTO users VALUES
+(1, 'admin', '$2y$10$c5otNVvQ06T9Ms6ZqHn0fuVeAONOYp6MIcEd.wkvWf8grlNN2Bnzu', '$2y$10$RHRVMWMJUjIyWNXlbynIfeDXiZ7z7IgO8wFSh4.IpseVaub/XgksS');
+
+-- 
+-- Восстановить предыдущий режим SQL (SQL mode)
+-- 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+
+-- 
+-- Включение внешних ключей
+-- 
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
